da1d2dc444c9337b9f5fe7240ae1e9de777c364f
Dennis Lutter <lad1337@gmail.com>
2020-01-21 07:53:53 +0100

    faking rarbg api

---
+++ plexarr/convert.py
@@ -0,0 +1,37 @@
+"""
+{
+    "title":"Off.Piste.2016.iNTERNAL.BDRip.x264-LiBRARiANS",
+    "category":"Movies/x264",
+    "download":"magnet:...",
+    "seeders":12,
+    "leechers":6,
+    "size":504519520,
+    "pubdate":"2017-05-21 02:13:49 +0000",
+    "episode_info":{
+        "imdb":"tt4443856",
+        "tvrage":null,
+        "tvdb":null,
+        "themoviedb":"430293"
+    },
+    "ranked":1,
+    "info_page":"https://torrentapi.org/...."
+}
+"""
+from plexapi.video import Movie
+from plexapi.media import Media
+
+
+def generate_scene_title(movie: Movie, media: Media):
+    return f"{movie.title} {movie.year} {media.videoResolution} Plexarr"
+
+
+def as_torrents(movie: Movie):
+    for media in movie.media:
+        yield dict(
+            title=generate_scene_title(movie, media),
+            category="Movies/x264",
+            download="\n".join([movie._server.url(f"{p.key}?download=1") for p in media.parts]),
+            seeders=100,
+            leachers=13,
+            size=sum(p.size for p in media.parts)
+        )
--- plexarr/server.py
+++ plexarr/server.py
@@ -1,11 +1,18 @@
 import os
 
 from fastapi import FastAPI
+from starlette.requests import Request
 from plexapi.myplex import MyPlexAccount
 from plexapi.exceptions import NotFound
+from plexapi.video import Movie
+from requests import request
+
+from plexarr.convert import as_torrents
 
 app = FastAPI()
 account = MyPlexAccount(os.getenv('PLEX_USERNAME'), os.getenv('PLEX_PASSWORD'))
+radarr_url = "{}/api".format(getenv('RADARR_URL'), )
+radarr_api_key = getenv('RADARR_URL')
 
 
 @app.get("/")
@@ -37,3 +44,28 @@ async def search(q: str = ''):
                 } for i in con.search(q)
         ])
     return results
+
+
+@app.get("/pubapi_v2.php")
+async def rarbg_fake(r: Request, mode: str = None, imdbId: str = None, search_string: str = None,
+                     get_token: str = None):
+    if get_token is not None:
+        return {"token": "1337"}
+    elif mode == 'list':
+        torrents = ['foo']
+        zeta = account.resource('zeta').connect()
+        movies = zeta.library.recentlyAdded()
+    else:
+        import pdb; pdb.set_trace()
+        request.get()
+        zeta = account.resource('zeta').connect()
+        movies = zeta.search(search_string)
+
+    torrents = []
+    for m in movies:
+        if isinstance(m, Movie):
+            torrents.extend(as_torrents(m))
+
+    return dict(
+        torrent_results=torrents
+    )
